78a9eaab6aff516f14110b478d970edc24332b4a
Moniruzzman Saikat <saikat.moniruzzman@gmail.com>
2022-01-30 23:43:10 +0600

    fixed cors

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
 
 // dev dependencies 
-if (process.env.NODE_ENV === '') {
+if (process.env.NODE_ENV === 'development') {
   app.use(require('morgan')('dev'));
 }
 
@@ -20,10 +22,6 @@ if (process.env.NODE_ENV === '') {
 const authRoutes = require('./routes/auth');
 const coursesRoutes = require('./routes/videos');
 const discussionRoutes = require('./routes/discussion');
-const { db } = require('./model/User');
-const User = require('./model/User');
-const Discussion = require('./model/Discussion');
-const Video = require('./model/Video');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/videos', checkAuth, coursesRoutes);
--- client/src/adapter/api.js
+++ client/src/adapter/api.js
@@ -3,7 +3,7 @@ import Axios from 'axios';
 const env = 'production';
 
 export const api = Axios.create({
-  baseURL: env === 'development' ? 'http://localhost:5000/api' : '/api',
+  baseURL: env === 'deveopment' ? 'http://localhost:5000/api' : 'https://course-buzz.herokuapp.com/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
